0a6f9d77c543c9e0c2a7af63b95760220e648a0e
Pakapol daengmanikun <6510110334@email.psu.ac.th>
2023-12-17 15:37:36 +0700

    add pylint test

---
+++ # pylint_test.py
@@ -0,0 +1,19 @@
+# pylint_test.py
+
+import os
+import subprocess
+
+def run_pylint():
+
+    python_files = [file for file in os.listdir() if file.endswith('.py')]
+
+    if not python_files:
+        print("No Python files found.")
+        return
+
+    for python_file in python_files:
+        file_path = os.path.join(os.getcwd(), python_file)
+        subprocess.run(['pylint', file_path], check=True)
+
+if __name__ == "__main__":
+    run_pylint()
